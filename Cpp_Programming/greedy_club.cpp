c76190373f6a5981b3fd2808b7c22f80cf5c9166
HYUNWOO PARK <95323172+k-motor@users.noreply.github.com>
2022-03-08 10:20:59 +0900

    Add files via upload

---
+++ Cpp_Programming/greedy_club.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int n;
+vector<int> arr;
+
+int main(void){
+	cin >> n;
+	
+	for(int i = 0; i<n; i++){
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	
+	sort(arr.begin(), arr.end() );
+	
+	int result = 0;
+	int count = 0;
+	
+	for(int i = 0; i<n; i++){
+		count += 1;
+		if(count >= arr[i]){
+			cout << count << ' ' << arr[i] << '\n';
+			result += 1;
+			count = 0;
+		}
+	}
+	
+	cout << result << '\n';
+}
---
+++ Cpp_Programming/greedy_mul_or_add.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+string str;
+
+int main(void){
+	cin >> str;
+	
+	long long result = str[0] - '0';
+	
+	for(int i = 1; i<str.size(); i++){
+		int num = str[i] - '0';
+		if(num <= 1 || result <=1){
+			result += num;
+		}else{
+			result *= num;
+		}
+	}
+	
+	cout << result << '\n';
+}
